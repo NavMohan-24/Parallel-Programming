3d504a741483a06e26d1f448dc468da280e7b287
Navaneeth Mohan <navaneethkrishnan24@gmail.com>
2025-09-01 07:33:58 +0530

    TFIM header files

---
+++ MPI/eigenvalue-computations/ising.hpp
@@ -0,0 +1,10 @@
+#ifndef ISING_HPP
+#define ISING_HPP
+
+#include <vector>
+#include <iostream>
+
+// forward declaration - telling compiler that the function exist somewhere.
+std::vector<std::vector<double>> tfimHamiltonian(int N, double J, double h);
+
+#endif
