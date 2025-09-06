2b3f7da94a099cdc7ccad8762017ce2ea883ac68
Navaneeth Mohan <navaneethkrishnan24@gmail.com>
2025-09-06 08:11:53 +0530

    modifying code to work with complex datatype

--- MPI/eigenvalue-computations/applying_commutator.cpp
+++ MPI/eigenvalue-computations/applying_commutator.cpp
@@ -1,27 +1,34 @@
 #include "ising.hpp"
+#include "random_rho.hpp"
 #include <vector>
 #include <cblas.h>
+#include <iomanip>
+
+using Complex = std::complex<double>;
 
 
-std::vector<double> applyCommutator(std::vector<double> H, std::vector<double> rho, int N){
+std::vector<Complex> applyCommutator(std::vector<Complex> H, std::vector<Complex> rho, int N){
     
-    double alpha = 1.0, beta = 0.0;
+    Complex alpha = {1.0,0.0};
+    Complex beta =  {1.0,0.0};
     int num_states = 1 << N;
 
-    std::vector<double> A(num_states*num_states);
-    std::vector<double> B(num_states*num_states);
-    std::vector<double> C(num_states*num_states);
+    std::vector<Complex> A(num_states*num_states);
+    std::vector<Complex> B(num_states*num_states);
+    std::vector<Complex> C(num_states*num_states);
 
     // A = H * rho
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
-        num_states, num_states, num_states, alpha, H.data(),
-        num_states, rho.data(), num_states, beta, A.data(), num_states
+    cblas_zgemm(CblasRowMajor, CblasNoTrans, CblasConjTrans,
+        num_states, num_states, num_states, &alpha, H.data(),
+        num_states, rho.data(), num_states, &beta, A.data(), 
+        num_states
     );
 
     // B = rho * H
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
-        num_states, num_states, num_states, alpha, rho.data(),
-        num_states, H.data(), num_states, beta, B.data(), num_states
+    cblas_zgemm(CblasRowMajor, CblasNoTrans, CblasConjTrans,
+        num_states, num_states, num_states, &alpha, rho.data(),
+        num_states, H.data(), num_states, &beta, B.data(),
+        num_states
     );
 
     // C = A - B
@@ -41,20 +48,20 @@ int main(){
     double J = 1.0; 
     double h = 1.0;
 
-    std::vector<double> rho(num_states*num_states);
+    std::vector<Complex> rho(num_states*num_states);
 
     std::cout << "Printing rho matrix.." << std::endl;
     for (int i = 0; i < num_states; i++){
         for (int j = 0; j < num_states; j++){
-            rho[i*num_states+j] = (i==j) ? 1:0;
+            rho[i*num_states+j] = Complex((i==j) ? 1:0,0);
             std::cout << rho[i*num_states+j] << " ";
         }
         std::cout << std::endl;
     }
 
-    std::vector<double> H = tfimHamiltonian(N,J,h);
+    std::vector<Complex> H = tfimHamiltonian(N,J,h);
     // std::vector<double> rho = constructRandomRho(N);
-    std::vector<double> matrix = applyCommutator(H,rho,N);
+    std::vector<Complex> matrix = applyCommutator(H,rho,N);
 
     
     
