559e4b8f7f9458f9490090e9896300e22d932d16
Navaneeth Mohan <navaneethkrishnan24@gmail.com>
2025-09-06 07:31:44 +0530

    refactor code to make matrix elements complex

--- MPI/eigenvalue-computations/ising_hamiltonian.cpp
+++ MPI/eigenvalue-computations/ising_hamiltonian.cpp
@@ -1,30 +1,31 @@
 #include "ising.hpp"
 #include <boost/dynamic_bitset.hpp>
+#include <iomanip>
 
-std::vector<double>  tfimHamiltonian(int N, double J, double h){
+std::vector<Complex>  tfimHamiltonian(int N, double J, double h){
 
     // int num_states = (int) std::pow(2, N); - risky and can have floating imprecision
 
     int num_states = 1 << N;
 
-    std::vector<double> hamiltonian(num_states*num_states);
+    std::vector<Complex> hamiltonian(num_states*num_states);
 
     for (int k = 0 ; k < num_states; k++){
         for (int l = 0; l < num_states; l++){
-            double ham = 0.0;
+            Complex ham(0.0,0.0);
             if (l == k){
                 boost::dynamic_bitset<> b(N, k);
                 for (int i = 0; i < N - 1; i++){
-                    ham += (b[i]==b[i+1]) ? -J:J;
+                    ham += Complex((b[i]==b[i+1]) ? -J:J,0);
                 }
             }
             else{
-                boost::dynamic_bitset<> bl(N, l);
+                boost::dynamic_bitset<> bl(N, l); 
                 boost::dynamic_bitset<> bk(N, k);
                 boost::dynamic_bitset<> c = bl ^ bk;
-                ham = (c.count()==1) ? -h:0; 
+                ham = Complex((c.count()==1) ? -h:0,0); 
             }
-            hamiltonian[l*num_states+k] = ham;
+            hamiltonian[k*num_states+l] = ham;
         }
     }
     return hamiltonian;
@@ -39,11 +40,12 @@ int main(){
     double J = 1.0;
     double h = 1.0;
 
-    std::vector<double> matrix = tfimHamiltonian(N, J, h);
+    std::vector<Complex> matrix = tfimHamiltonian(N, J, h);
 
     for (int i = 0; i < num_states; i++){
         for (int j = 0; j < num_states; j++){
-            std::cout << matrix[i*num_states+j] << " "; 
+            std::cout << std::fixed << std::setprecision(2) 
+          << matrix[i*num_states + j] << " "; 
         }
          std::cout << std::endl;
     }
