bc50b90ecafe6f1bc7d00b01eef71849b2744a05
Navaneeth Mohan <navaneethkrishnan24@gmail.com>
2025-09-06 06:35:50 +0530

    adding type alias of complex

--- MPI/eigenvalue-computations/ising.hpp
+++ MPI/eigenvalue-computations/ising.hpp
@@ -4,6 +4,9 @@
 #include <vector>
 #include <iostream>
 
+//type alias complex data type
+using Complex = std::complex<double>;
+
 // forward declaration - telling compiler that the function exist somewhere.
 std::vector<double> tfimHamiltonian(int N, double J, double h);
 
