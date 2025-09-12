822eeaea856f11ac68fdc30412464c03bde5e14a
Navaneeth Mohan <navaneethkrishnan24@gmail.com>
2025-09-12 06:55:14 +0530

    initial commit

---
+++ MPI/eigenvalue-computations/jump_operator.cpp
@@ -0,0 +1,23 @@
+#include "jump_op.hpp"
+
+enum class Scope {Local};
+enum class DecayType {SigmaMinus, SigmaPlus, Dephasing};
+
+std::vector<Complex> constructJumpOperator(int num_states, Scope scope, DecayType type){
+
+    switch(scope)
+    {
+    if (type == DecayType::SigmaMinus){
+
+    };
+    if (type == DecayType::SigmaPlus){
+        break;
+
+    };
+    if (type == DecayType::Dephasing){
+        break;
+    };
+
+    };
+   
+}
