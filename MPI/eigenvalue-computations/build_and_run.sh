ceca7c857963279abe61b296c4555b94f028fbc1
Navaneeth Mohan <navaneethkrishnan24@gmail.com>
2025-11-18 18:52:59 +0530

    update diagonalizer path

--- MPI/eigenvalue-computations/build_and_run.sh
+++ MPI/eigenvalue-computations/build_and_run.sh
@@ -19,7 +19,7 @@ g++-15 -c -fopenmp ./jump_operator/jump_operator.cpp -o ./bin/jump_operator.o
 g++-15 -c -fopenmp ./linbladian_constructor/linbladian_constructor.cpp -o ./bin/linbladian.o 
 
 # compile diagonalizer
-g++-15 -c ./linbladian_diagonalizer.cpp -o ./bin/diagonalizer.o 
+g++-15 -c ./arnoldi_diagonalizer/linbladian_diagonalizer.cpp -o ./bin/diagonalizer.o 
 
 # run function to find liouvillian spectrum of TFIM
 g++-15 -fopenmp tfim_linbladian_spectrum.cpp ./bin/diagonalizer.o ./bin/linbladian.o ./bin/density_matrix.o ./bin/jump_operator.o ./bin/hamiltonian.o -o res_spectrum.out -lblas -llapacke -std=c++20
